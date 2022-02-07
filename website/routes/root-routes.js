0514e979f8bd30b1937eef3e2a074670568a7eb5
woterr <shreyasv320@gmail.com>
2022-02-07 23:14:33 +0530

    added tos and priacy policy

--- website/routes/root-routes.js
+++ website/routes/root-routes.js
@@ -9,5 +9,7 @@ router.get("/", (req, res) =>
 );
 router.get(`/setup`, (req, res) => res.render(`setup`));
 router.get(`/faq`, (req, res) => res.render(`faq`));
+router.get(`/terms`, (req, res) => res.render(`tos`));
+router.get(`/privacy`, (req, res) => res.render(`privacy`));
 
 module.exports = router;
