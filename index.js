aaddcb71c2cd762c6ff7de78df938988ca00a3cb
woterr <shreyasv320@gmail.com>
2022-05-28 22:05:23 +0530

    seperate web

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require("express");
 const app = express();
-
-const rootRoutes = require(`./routes/root-routes.js`);
-const authRoutes = require(`./routes/auth-routes.js`);
-const cookies = require("cookies");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
 const port = process.env.PORT || 3000;
 const { Client, Collection } = require("discord.js");
 require("dotenv").config();
 
-__dirname;	
-app.set("views", __dirname + "/views");
-app.set("view engine", "pug");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(cookies.express("a", "b", "c"));
-
-app.use(express.static(`${__dirname}/assets`));
-app.locals.basedir = `${__dirname}/assets`;
-
-app.use(
-  "/",
-  rootRoutes,
-  authRoutes,
-);
-
-app.all("*", (req, res) => res.render("404"));
+router.get(`/`, (req, res) => res.send("get a job"));
 
 app.listen(port, () => console.log(`Server is live on port 3000`));
 
@@ -48,10 +26,6 @@ client.slashCommands = new Collection();
 // Initializing the project
 require("./handler")(client);
 
-
-// APp
-
-module.exports = app;
 
 module.exports = client;
 client.login(process.env.token);
